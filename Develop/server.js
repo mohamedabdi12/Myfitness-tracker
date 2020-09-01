7644ffc3cf7bc2f4d8684880ccd4f2e3df5ad2d8
Mohamed Abdi <lovegoldone@gmail>
2020-08-31 18:00:02 -0700

    created and coded all the required files and folders

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {useNewUrlParser: true});
+
+
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
