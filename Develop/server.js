eed21d90814ea73b2f53f210b1e1b30c1910c2f7
Mohamed Abdi <lovegoldone@gmail>
2020-09-04 20:25:02 -0700

    ready

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myfitnesstracker", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myfitness-tracker", { useNewUrlParser: true });
 
 
 require('./routes/api-routes')(app)
